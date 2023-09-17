8f434e53eaeff5c14133fada4f0f8b16aef575a4
ZainAalabidin <zynly9303@gmail.com>
2023-09-17 11:20:47 -0700

    T0

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFF_SIZE 1024
-void print_buffer(char buffer[], int *buff_ind); 
 int _printf(const char *format, ...);
 int print_string(char *str);
 int print_char(char c);
