2b5903e7eb52f8448a48fb1eca5e5b66e42a236a
ZainAalabidin <zynly9303@gmail.com>
2023-09-17 10:01:00 -0700

    edit printf and add another prototype in main.h

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#define BUFF_SIZE 1024
+void print_buffer(char buffer[], int *buff_ind); 
 int _printf(const char *format, ...);
 int print_string(char *str);
 int print_char(char c);
